2222edd23f6614a32c1bb797070abeb91b0efa26
wawzat <wawzat@gmail.com>
2021-04-17 18:16:47 -0700

    Debug - Try printing plain text string

--- now_playing.py
+++ now_playing.py
@@ -141,7 +141,8 @@ try:
     sleep(4)
     token = spotify_authenticate()
     while True:
-        track_name = get_track(token)
+        track_name = "The presidents of the united states of America"
+        #track_name = get_track(token)
         print(track_name)
         led_write_time_2 = write_matrix(track_name, "0", led_write_time_2)
         sleep(15)
